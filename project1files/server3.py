021dc530422c5b51243805ebaee43dca31b15f0e
Minnie Smith <jacta-alea-est@hotmail.com>
2021-05-04 17:09:49 +0800

    Maybe I'm going down the wrong rabbit hole!

--- project1files/server3.py
+++ project1files/server3.py
@@ -9,34 +9,57 @@ class Player():
     def __init__(self, connection: socket, idnum: int, address: str):
         self.conection = connection
         self.idnum = idnum
-        self.initial_connection = True
         self.address = address
         self.host, self.port = address
         self.turn = False
+        self.initial_connection = True
+        self.deal_hand = False
+        self.selected_as_player = False
         self.name = '{}:{}'.format(self.host, self.port)
         self.msg = None
 
 
-def initial_connection_deal_hand(player: Player):
-    logging.info(f'Initial connection player = {player.idnum}')
-    player.conection.send(tiles.MessageWelcome(player.idnum).pack())
-    logging.info(f'Server send tiles.MessageWelcome idnum = {player.idnum}')
-    for p in players:
-        p.conection.send(tiles.MessagePlayerJoined(player.name, player.idnum).pack())
-    player.conection.send(tiles.MessageGameStart().pack())
+def add_clients_to_connected_list():
+    potential_player = Player(connection, idnum, client_address)
+    connected_clients.append(potential_player)
+    outputs.append(potential_player.conection)
+    logging.info(f'Client {potential_player.idnum} appended to connected clients list')
 
-    for _ in range(tiles.HAND_SIZE):
-        tileid = tiles.get_random_tileid()
-        player.conection.send(tiles.MessageAddTileToHand(tileid).pack())
 
-    player.conection.send(tiles.MessagePlayerTurn(player.idnum).pack())
-    player.initial_connection = False
+def send_connection_idnum_and_welcome():
+    for c in connected_clients:
+        c.conection.send(tiles.MessageWelcome(c.idnum).pack())
+        logging.info(f'Server send tiles.MessageWelcome idnum = {c.idnum}')
 
+def add_client_to_players_list():
+        new_player = connected_clients.pop(0)
+        players.append(new_player)
+        live_idnums.append(new_player.idnum)
+        new_player.selected_as_player = True
+        new_player.conection.setblocking(False)
+        logging.info(f'Initial connection player = {new_player.idnum}')
+        # connection added to the list of sockets to monitor for input
+        inputs.append(new_player.conection)
+        logging.info(f'Player {new_player.idnum} appended to players list')
+        outputs.append(new_player.conection)
+        logging.info(f'Player {new_player.idnum} added to outputs list')
+        new_player.selected_as_player = True
+
+
+def start_game():
+    for player in players:
+        player.conection.send(tiles.MessagePlayerJoined(player.name, player.idnum).pack())
+        logging.info(f'Player {player.idnum} has joined the game')
+        player.conection.send(tiles.MessageGameStart().pack())
+
+        for _ in range(tiles.HAND_SIZE):
+            tileid = tiles.get_random_tileid()
+            player.conection.send(tiles.MessageAddTileToHand(tileid).pack())
+
+        player.conection.send(tiles.MessagePlayerTurn(player.idnum).pack())
+        outputs.remove(player.conection)
 
 def player_moves(player: Player):
-    live_idnums = []
-    for p in players:
-        live_idnums.append(p.idnum)
 
     if isinstance(player.msg, tiles.MessagePlaceTile):
         if board.set_tile(player.msg.x, player.msg.y, player.msg.tileid, player.msg.rotation, player.msg.idnum):
@@ -102,7 +125,7 @@ sock.bind(server_address)
 # set server to non-blocking
 sock.setblocking(False)
 logging.info(f'Listening on port {sock.getsockname()}')
-sock.listen(5)
+sock.listen(10)
 
 # Sockets from which we expect to read
 inputs = [sock]
@@ -110,8 +133,12 @@ inputs = [sock]
 outputs = [sock]
 
 idnum = 0
+live_idnums = []
+connected_clients = []
 players = []
 board = tiles.Board()
+game_started = False
+clients_sent_welcome = False
 
 
 buffer = bytearray()
@@ -120,23 +147,17 @@ while True:
     # must give a time out value to select or else it will act in a blocking manner
     readable, writable, errors = select.select(inputs, outputs, inputs, 0.5)
 
+# --------------------------------------------------------------------------------------------
     for s in readable:
-        logging.info('READABLE')
+        # logging.info('READABLE')
         try:
             if s == sock:
+                # establish connection and add client to potential player list
                 connection, client_address = s.accept()
-                # set clients to non-blocking as well
-                connection.setblocking(False)
-                # connection added to the list of sockets to monitor for input
-                inputs.append(connection)
-                logging.info(f'Client Connection: {client_address}')
-                # client establishes connection - deal tiles
                 idnum += 1
-                conn = Player(connection, idnum, client_address)
-                players.append(conn)
-                logging.info(f'Player {conn.idnum} appended to players list')
-                outputs.append(connection)
-
+                # add client to clients list
+                add_clients_to_connected_list()
+                # s appended to output list
             else:
                 data = s.recv(4096)
                 if data:
@@ -156,7 +177,6 @@ while True:
 
                 # if we don't receive any data
                 else:
-
                     for p in players:
                         if p.conection == s:
                             players.remove(p)
@@ -166,26 +186,55 @@ while True:
         except Exception as ex:
             logging.warning(ex.args)
 
+# --------------------------------------------------------------------------------------------
     for s in writable:
-        logging.info('WRITABLE')
-        live_idnums = []
-        for p in players:
-            live_idnums.append(p.idnum)
-        logging.info(f'Players List = {live_idnums}')
-        conn = Player
+        # logging.info('WRITABLE')
+        # send all connected clients their unique idnums
+        if not clients_sent_welcome:
+            for c in connected_clients:
+                if c.conection == s:
+                    if c.initial_connection:
+                        send_connection_idnum_and_welcome()
+                        c.initial_connection = False
+                        outputs.remove(s)
+                        logging.info(f'Client {c.idnum} has been removed from output list')
+            clients_sent_welcome = True
 
-        for p in players:
-            if s == p.conection:
-                conn = p
-                if p.initial_connection:
-                    initial_connection_deal_hand(p)
-                else:
-                    player_moves(p)
-                    p.msg = None
-                outputs.remove(s)
+        # if not game_started:
+        # #   start game and deal hand
+        #     start_game()
+        #     game_started = True
+
+
+
+
+
+        # for p in players:
+        #     if p.conection == s:
+        #         if not game_started and len(players) == 4:
+        #             for q in players:
+        #                 start_game(q)
+        #                 outputs.remove(q.connection)
+        #             game_started = True
+
+                    # else:
+                    #     player_moves(p)
+                    #     logging.info('TODO: player_moves()')
+                    #     p.msg = None
+        # for p in players:
+        #     if not game_started:
+        #         if len(players) == 4:
+        #             start_game(p)
+        #             game_started = True
+        #     else:
+        #         player_moves(p)
+        #         logging.info('TODO: player_moves()')
+        #         p.msg = None
+        #
+        # outputs.remove(s)
 
     for s in errors:
-        logging.info('ERROR')
+        # logging.info('ERROR')
         logging.info(f'handling exceptional condition for {s.getpeername()}')
         # Stop listening for input on the connection
         inputs.remove(s)
